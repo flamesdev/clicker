e445a4dee1285acdb1a18cbb271f1711162211ac
flamesdev <johannpayer123@gmail.com>
2020-10-16 00:55:09 -0400

    First upload

---
+++ README.md
@@ -0,0 +1,2 @@
+# clicker
+A simple clicker game with around 8-10 minutes of gameplay.
---
+++ script.js
@@ -0,0 +1,50 @@
+let points;
+let pointsPerClick;
+let consecutiveClicks;
+let ascension = 0;
+let didWin = false;
+const goal = 100000;
+
+resetLevel();
+
+function update() {
+  if (didWin) {
+    header.innerHTML = '🏆';
+    subheader.hidden = true;
+  } else {
+    header.innerHTML = points;
+    subheader.innerHTML = `+${pointsPerClick} ｜ ${Math.floor(points / goal * 100)}% ｜ ${ascension}a`;
+  }
+}
+
+function resetLevel() {
+  points = 0;
+  pointsPerClick = 1;
+  consecutiveClicks = 0;
+}
+
+window.onload = update;
+window.onclick = () => {
+  if (!didWin) {
+    points += pointsPerClick;
+
+    if (points >= goal) {
+      resetLevel();
+      ascension++;
+
+      if (ascension >= 20) {
+        didWin = true;
+      }
+    } else {
+      consecutiveClicks += 2 ** ascension;
+
+      const limit = 10;
+      if (consecutiveClicks >= limit) {
+        pointsPerClick += Math.floor(consecutiveClicks / limit);
+        consecutiveClicks %= limit;
+      }
+    }
+
+    update();
+  }
+};
